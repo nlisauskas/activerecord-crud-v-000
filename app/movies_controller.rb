1ff85e7a23dcdd022db46b83ee4ff0986c88dd0b
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-11 16:34:38 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -73,7 +73,7 @@ def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
   Movie.update(:title => "Wat, huh?")
-  __
+  Movie.self
 end
 
 def can_update_multiple_items_at_once
